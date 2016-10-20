db89994fa01374b6a55198af0305c45eb6b72ca2
Maria163 <Mariaz12@yandex.ru>
2016-10-20 19:54:18 +0400

    HelloWorld

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,6 +8,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-        }
-    }
+            Console.WriteLine("Hello World");
+            string userName;
+            if (args. Length > 0)
+
+            {
+        else
+            }
+            Console.Write ("What's your name?");
+             userName = Console.ReadLine ();
+ 
+    };
+
+             Console.WriteLine ("Hello, " + userName);
+             Console.Write("\nPrint any key to exit");
+             Console.ReadKey ();
+           
+
+}
 }
