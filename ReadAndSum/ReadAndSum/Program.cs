5117777e22cb5411842d7ed80a9bd6e4f746134d
Maria163 <Mariaz12@yandex.ru>
2016-10-20 20:16:01 +0400

    Лекция2;Пример1. Сложение двух чисел

---
+++ ReadAndSum/ReadAndSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.Readline(), out b);
+
+
+            int sum = a + b;
+            Console.WriteLine(a + "+" + b + "=" + sum);
+            Console.ReadKey();
+
+        }
+
+    }
+}
