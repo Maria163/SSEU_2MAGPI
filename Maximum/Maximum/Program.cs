7c976b85286d7b2d7dc8089c15fde697b256c28c
Maria163 <Mariaz12@yandex.ru>
2016-10-20 20:38:28 +0400

    Лекция2; Пример2; Максимум

---
+++ Maximum/Maximum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод исходных значений 
+            int A, B;
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.ReadLine(), out b);
+            
+            // максимальное значение
+            int Max;
+
+            // если больше А
+            if (A >= B)
+            {
+                Max = A;
+            }
+            //если больше В
+            else
+            {
+                Max = B;
+            }
+          //вывод результата
+            Console.WriteLine("Максимальное =" + Max);
+            //завершение программы
+            Console.ReadKey();
+
+
+    
+
+        }
+    }
+}
