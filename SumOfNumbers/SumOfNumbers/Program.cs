a9286fd90dbbeb9b102f84d1cf76e864ae79bd17
Maria163 <Mariaz12@yandex.ru>
2016-12-16 19:00:50 +0400

    Лекция 3, пример 1 Сложение N  целых чисел

---
+++ SumOfNumbers/SumOfNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                int N;
+                int x; // одно число
+                if ( int.TryParse(Console.ReadLine (), out N))
+                {
+                    int Sum = 0; 
+                    for (int i = 0; i < N; i ++)
+                        if (int.TryParse(Console.ReadLine(), out x))
+                        {
+                            Sum += x; //добавить к сумме
+
+                        };
+                };
+
+
+
+            Console.WriteLine(+Sum);
+           
+
+            Console.ReadKey();
+
+        }
+    }
+}
